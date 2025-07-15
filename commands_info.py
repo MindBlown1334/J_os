a873ef739bdb33e360fae7fe05e69dbabfc9e70b
MindBlown1334 <jakubbiernacki173343106@gmail.com>
2025-07-15 14:42:45 +0200

    Create commands_info.py

---
+++ commands_info.py
@@ -0,0 +1,9 @@
+INFO = [
+    "| >COMMAND< |     | >ARGUMENT<      |     | >DESCRIPTION<                 |",
+    "| \\help     | --> | [ NO ARGUMENT ] | --> | SHOWS THIS WINDOW             |",
+    "| \\quit     | --> | [ NO ARGUMENT ] | --> | QUITS PROGRAM                 |",
+    "| \\color    | --> | [ 0F A0  ETC. ] | --> | CHANGES MENU COLOR            |",
+    "| \\create   | --> | [ FILE / DIR  ] | --> | CREATE NEW FILE / DIRECTORY   |",
+    "| \\delete   | --> | [ FILE / DIR  ] | --> | DEALATE FILE / DIRECTORY      |",
+    "| \\edit     | --> | [ FILE / DIR  ] | --> | EDIT FILES                    |",
+]
